8f56c653d66c284bd5e9f5df87eb357c7525ec76
Aman <142656811+Amanthisside@users.noreply.github.com>
2024-09-07 23:08:45 +0530

    Update index.js

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,28 +1,23 @@
-const cloudinary=require('cloudinary').v2;
-const {CloudinaryStorage}= require('multer-storage-cloudinary');
-const express= require('express');
-const multer=require('multer');
-
-const app=express();
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_KEY,
-    api_secret:process.env.CLOUDINARY_SECRET
-})
+    api_secret: process.env.CLOUDINARY_SECRET
+});
 
-const storage= new CloudinaryStorage({
+const storage = new CloudinaryStorage({
     cloudinary,
-    params:{
-        folder:'YelpCamp',
-        allowedFormats:['jpeg','png','jpg']
+    params: {
+        folder: 'YelpCamp',
+        allowedFormats: ['jpeg', 'png', 'jpg']
     }
 });
 
-module.exports={
+module.exports = {
     cloudinary,
     storage
 }
-
 //for show.ejs images
 // <!-- <img src="<%=img.url%>" class="card-img-top" alt="..."> -->
